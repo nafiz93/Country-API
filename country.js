4419acdbecb6646694bdde6b88c8d29f66b33991
nafiz93 <nafiz93@gmail.com>
2024-06-20 16:53:34 +0200

    set the display

--- country.js
+++ country.js
@@ -18,37 +18,12 @@ const display = countries => {
         const homeInterface = document.createElement('div');
         homeInterface.className = 'homeStyle';
         homeInterface.innerHTML = `<h1 class='name'>${country.name.common}</h1>
-            <p>${country.ccn3}</p>
-            <p>${country.continents[0]}</p>
-            <img style="display: none;" src="${country.flags.png}" alt="">
-            
-            <button onclick="showInTheCurrentDiv(this)">view Flag</button>`;
+                <p>${country.ccn3}</p>
+                <p>${country.continents[0]}</p>
+                <img style="display: none;" src="${country.flags.png}" alt="">
+                
+                <button onclick="showInTheCurrentDiv(this)">view Flag</button>`;
         parent.appendChild(homeInterface);
     });
 };
 
-const show = (img, name) => {
-    const details = document.getElementById("details");
-
-    const existingDiv = details.querySelector('#work');
-
-    if (existingDiv) {
-        details.removeChild(existingDiv);
-    }
-
-    const image = document.createElement('div');
-    image.id = 'work';
-
-    image.innerHTML = `<h1>${name}</h1>
-        <img src="${img}" alt="">`;
-    details.appendChild(image);
-};
-
-
-
-const showInTheCurrentDiv = button => {
-    parentDiv = button.parentNode;
-    const image = parentDiv.querySelector('img');
-    image.style.display = 'block'
-
-}
